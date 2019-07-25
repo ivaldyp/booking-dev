1e3b6c4c1303c91818ea5f9973c0587b398ddc10
ivaldyp <vivaaldy@gmail.com>
2019-07-25 14:17:43 +0700

    homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,111 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
+    
+    <section class="content">
+      <div class="row">
+        <?php echo $user_status; ?>
+      </div>
+      <div class="row">
+        <!-- /.col -->
+        <div class="col-md-2"></div>
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <?php echo $user_status; ?> 
-                    You are logged in!
-                </div>
+          <div class="box box-primary">
+            <div class="box-body no-padding">
+              <!-- THE CALENDAR -->
+              <div id="calendar"></div>
             </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /. box -->
         </div>
-    </div>
-</div>
+        <div class="col-md-2"></div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+
+    <script language="javascript" type="text/javascript">
+      $(document).ready(function () {
+        /* initialize the calendar
+         -----------------------------------------------------------------*/
+        //Date for the calendar events (dummy data)
+
+        var date = new Date();
+        var d = date.getDate(),
+            m = date.getMonth(),
+            y = date.getFullYear();
+        var testt = 2;
+        $('#calendar').fullCalendar({
+          weekends: false,
+          header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+          },
+          buttonText: {
+            today: 'today',
+            month: 'month',
+            week: 'week',
+            day: 'day'
+          }
+          // ,
+          // events: [
+          //   <?php for($i=0; $i< "+testt+"; $i++) { ?>
+          //   {
+          //     title: "<?php echo($i); ?>",
+          //     start: '2019-07-24',
+          //   },
+          //   <?php } ?>
+          // ]
+          ,
+          dayClick: function(info) {
+            alert('a day has been clicked!');
+          },
+          // eventClick: function(info) {
+          //   var eventObj = info.event;
+
+          //   if (eventObj) {
+          //     alert(
+          //       'Clicked ' + eventObj.title + '.\n'
+          //       // 'Will open ' + eventObj.url + ' in a new tab'
+          //     );
+
+          //     // window.open(eventObj.url);
+
+          //     info.jsEvent.preventDefault(); // prevents browser from following link in current tab.
+          //   } else {
+          //     alert('Clicked ' + eventObj.title);
+          //   }
+          // },
+        });
+
+        var datas;
+        // var newEvent = [];
+        $.ajax({
+          url: "/home/read",
+          type: "get",
+          success: function(data){
+            datas = data.bookings;
+
+            console.log(datas);
+            console.log(datas[0].book_hstart);
+            console.log(datas[0].booking_judul);
+            for(var i=0; i<datas.length; i++)
+            {
+              // newEvent.push({ title:datas[i].booking_judul, allDay: true });
+              var newEvent = {
+                title: datas[i].booking_judul,
+                start: (datas[i].booking_date + " " + datas[i].time_startname),
+                end: (datas[i].booking_date + " " + datas[i].time_endname)
+              };
+              $('#calendar').fullCalendar( 'renderEvent', newEvent , 'stick');    
+            }
+          }
+        });
+      });
+       
+    </script>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/registeruser', 'LoadRegisterController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home/read', 'HomeController@read');
