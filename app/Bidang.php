26601514d0b1805b5353c12e3df8577917fd0a96
ivaldyp <vivaaldy@gmail.com>
2019-11-19 08:44:38 +0700

    pindahin database migration dan seeds

--- app/Bidang.php
+++ app/Bidang.php
@@ -11,4 +11,11 @@ class Bidang extends Model
     protected $fillable = [
     	'bidang_name', 'created_at', 'updated_at'
     ];
+
+    public function subbidang()
+	{
+	    return $this->belongsTo('App\Subbidang', 'id_bidang', 'id_bidang');
+	}
+
+    
 }
--- app/Booking.php
+++ app/Booking.php
@@ -10,7 +10,7 @@ class Booking extends Model
     public $incrementing = false;
 
     protected $fillable = [
-        'id_booking', 'id_peminjam', 'nip_peminjam', 'nama_peminjam', 'bidang_peminjam, id_penyetuju', 'booking_room', 'booking_date', 'booking_total_tamu', 'id_surat', 'time_start', 'time_end', 'booking_status', 'keterangan_status', 'request_hapus', 'created_at', 'updated_at', 'soft_delete'
+        'id_booking', 'id_peminjam', 'nip_peminjam', 'nama_peminjam', 'bidang_peminjam, id_penyetuju', 'booking_room', 'booking_room_owner', 'booking_date', 'booking_total_tamu', 'id_surat', 'time_start', 'time_end', 'booking_status', 'keterangan_status', 'request_hapus', 'created_at', 'updated_at', 'soft_delete'
     ];
 
     public function bidang()
@@ -25,22 +25,22 @@ class Booking extends Model
 
     public function surat()
     {
-    	return $this->hasOne('App\Surat','id_surat','id_surat');
+        return $this->hasOne('App\Surat','id_surat','id_surat');
     }
 
     public function time1()
     {
-    	return $this->hasOne('App\Time', 'id_time', 'time_start');
+        return $this->hasOne('App\Time', 'id_time', 'time_start');
     }
 
     public function time2()
     {
-    	return $this->hasOne('App\Time', 'id_time', 'time_end');
+        return $this->hasOne('App\Time', 'id_time', 'time_end');
     }
 
     public function room()
     {
-    	return $this->hasOne('App\Room', 'id_room', 'booking_room');
+        return $this->hasOne('App\Room', 'id_room', 'booking_room');
     }
 
     public function user()
