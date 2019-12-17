e7d4e6c20533c6bc4f1da8acc06a8873927c581c
ivaldyp <vivaaldy@gmail.com>
2019-12-17 16:59:08 +0700

    push

--- resources/views/layouts/master2.blade.php
+++ resources/views/layouts/master2.blade.php
@@ -267,7 +267,7 @@
 
     <!-- jquery validation -->
     <!-- <script src="{{ ('/booking-dev/public/js/jquery-validation2/dist/jquery.validate.min.js') }}"></script> -->
-    <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
+   < script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
     
     <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.min.js"></script>
     
